2204179732442114c0420d169a90223cb05ffc0c
Christophe Moreira <moreira.christophe@outlook.com>
2019-11-01 21:09:23 +0100

    layout front page materialui

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import FrontPage from '../components/FrontPage'
+import Container from '@material-ui/core/Container';
+
+const Home = () => (
+  // <Container style={{height:'100vh', background: 'orange'}} >
+  <Container>
+
+    <div style={{height:'10vh'}}></div>
+    <FrontPage />
+    {/* <style global jsx>{`
+        html {
+          position: relative;
+        }
+      `}</style> */}
+      </Container>
+// </Container>
+)
+
+export default Home
